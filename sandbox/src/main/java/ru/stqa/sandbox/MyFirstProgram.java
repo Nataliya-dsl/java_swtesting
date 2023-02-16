426c30c311b935c9f8236d7bccc4c3aa4a0c332b
Nataliya <khmelnitskayanatali@gmail.com>
2023-02-16 18:40:54 +0100

    Add new methods

--- sandbox/src/main/java/ru/stqa/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/sandbox/MyFirstProgram.java
@@ -1,7 +1,28 @@
 package ru.stqa.sandbox;
 
 public class MyFirstProgram {
-    public static void main(String[] args){
-        System.out.println("Hello, world!");
+    public static void main(String[] args) {
+        printHello("world");
+        printHello("Natali");
+
+        double l = 5;
+        System.out.println("Area of square with a side " + l + " = " + area(l));
+
+        double a = 3;
+        double b = 4;
+        System.out.println("Area of rectangle with sides " + a + " and " + b + " = " + area(a,b));
+
+    }
+
+    public static void printHello(String somebody) {
+        System.out.println("Hello, " + somebody + "!");
+    }
+
+    public static double area(double l) {
+        return l * l;
+    }
+
+    public static double area(double a, double b) {
+        return a * b;
     }
 }
