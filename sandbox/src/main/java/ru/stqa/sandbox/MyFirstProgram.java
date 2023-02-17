a00f26e5dd903b442713c4cc237f647481f87c76
Nataliya <khmelnitskayanatali@gmail.com>
2023-02-17 13:07:32 +0100

    Task 2 - Add class Point and method distance

--- sandbox/src/main/java/ru/stqa/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/sandbox/MyFirstProgram.java
@@ -11,10 +11,16 @@ public class MyFirstProgram {
         Rectangle r = new Rectangle(6, 4);
         System.out.println("Area of rectangle with sides " + r.a + " and " + r.b + " = " + r.area());
 
+        Point p1 = new Point(2, 3);
+        Point p2 = new Point(5, 4);
+        System.out.println("Расстояние между точками (" + p1.x + ", " + p1.y + ") и  (" + p2.x + ", " + p2.y+ ") = " + p1.distance(p2));
+
+
     }
 
     public static void printHello(String somebody) {
         System.out.println("Hello, " + somebody + "!");
     }
+
 
 }
