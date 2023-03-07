caaa0d16460c93cb67c6266d74ff349271af38e0
Nataliya <khmelnitskayanatali@gmail.com>
2023-03-07 12:02:14 +0100

    change test project

--- sandbox/src/main/java/ru/stqa/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/sandbox/MyFirstProgram.java
@@ -11,8 +11,8 @@ public class MyFirstProgram {
         Rectangle r = new Rectangle(6, 4);
         System.out.println("Area of rectangle with sides " + r.a + " and " + r.b + " = " + r.area());
 
-        Point p1 = new Point(2, 3);
-        Point p2 = new Point(5, 4);
+        Point p1 = new Point(2, 4);
+        Point p2 = new Point(4, 6);
         System.out.println("Расстояние между точками (" + p1.x + ", " + p1.y + ") и  (" + p2.x + ", " + p2.y+ ") = " + p1.distance(p2));
 
 
--- sandbox/src/test/java/ru/stqa/sandbox/SquareTest.java
+++ sandbox/src/test/java/ru/stqa/sandbox/SquareTest.java
@@ -10,11 +10,4 @@ public class SquareTest {
         Square s = new Square(5);
         Assert.assertEquals(s.area(), 25.0);
     }
-
-    @Test
-    public void testPointDistance() {
-        Point p1 = new Point(2,3);
-        Point p2 = new Point(5,4);
-        Assert.assertEquals(Math.round(p1.distance(p2)), 3);
-    }
 }
