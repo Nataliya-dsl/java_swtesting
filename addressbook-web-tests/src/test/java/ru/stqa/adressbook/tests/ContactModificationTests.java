181cc6f9348338439f32d46bffcbaed7809a079e
Nataliya <khmelnitskayanatali@gmail.com>
2023-03-09 16:13:32 +0100

    New tests for deletion and modification contact list

---
+++ addressbook-web-tests/src/test/java/ru/stqa/adressbook/tests/ContactModificationTests.java
@@ -0,0 +1,18 @@
+package ru.stqa.adressbook.tests;
+
+import org.testng.annotations.Test;
+import ru.stqa.adressbook.model.NewContactDatas;
+
+public class ContactModificationTests extends TestBase {
+
+    @Test
+    public void testContactModification() {
+        app.getNavigationHelper().gotoHomePage();
+        app.getContactHelper().editContact();
+        app.getContactHelper().fillContactData(new NewContactDatas("Test", "Test", "Test", "testuser",
+                "TestCompany", "Country, City, Street, 0",
+                "+45123456789", "+987654321"));
+        app.getContactHelper().updateContactData();
+        app.getContactHelper().returnHomePage();
+    }
+}
