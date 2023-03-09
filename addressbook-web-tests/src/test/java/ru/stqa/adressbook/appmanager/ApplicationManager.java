90edeb5758f2de7c2f4e0476aa77ab402c9873ed
Nataliya <khmelnitskayanatali@gmail.com>
2023-03-09 13:28:59 +0100

    Перенос isAlertPresent

--- addressbook-web-tests/src/test/java/ru/stqa/adressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/adressbook/appmanager/ApplicationManager.java
@@ -1,7 +1,5 @@
 package ru.stqa.adressbook.appmanager;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.NoAlertPresentException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -30,16 +28,6 @@ public class ApplicationManager {
     public void stop() {
         wd.quit();
     }
-
-    public boolean isAlertPresent(By by) {
-        try {
-            wd.switchTo().alert();
-            return true;
-        } catch (NoAlertPresentException e) {
-            return false;
-        }
-    }
-
 
     public GroupHelper getGroupHelper() {
         return groupHelper;
