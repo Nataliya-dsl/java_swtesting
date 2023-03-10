a3292f47c4d2d73e8c79f491f8d91d15ee953e5f
Nataliya <khmelnitskayanatali@gmail.com>
2023-03-10 15:48:29 +0100

    Запуск тестов в разных браузерах

--- addressbook-web-tests/src/test/java/ru/stqa/adressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/adressbook/appmanager/ApplicationManager.java
@@ -2,19 +2,36 @@ package ru.stqa.adressbook.appmanager;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.Browser;
+import org.openqa.selenium.safari.SafariDriver;
 
 import java.time.Duration;
 
 public class ApplicationManager {
 
+    private String browser;
     public WebDriver wd;
     private ContactHelper contactHelper;
     private SessionHelper sessionHelper;
     private NavigationHelper navigationHelper;
     private GroupHelper groupHelper;
 
+    public ApplicationManager(String browser) {
+        this.browser = browser;
+    }
+
     public void init() {
-        wd = new ChromeDriver();
+        if (Browser.CHROME.browserName().equals(browser)) {
+            wd = new ChromeDriver(new ChromeOptions().addArguments("--remote-allow-origins=*"));
+        } else if (Browser.SAFARI.browserName().equals(browser)) {
+            wd = new SafariDriver();
+        } else {
+            wd = new FirefoxDriver();
+        }
+
+
         wd.manage().timeouts().implicitlyWait(Duration.ofDays((20)));
         wd.get("http://localhost/addressbook/index.php");
         groupHelper = new GroupHelper(wd);
