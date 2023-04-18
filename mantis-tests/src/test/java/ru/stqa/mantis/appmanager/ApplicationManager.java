b619027be72297751c7c8f48d45da17a72917456
Nataliya <khmelnitskayanatali@gmail.com>
2023-04-18 13:51:54 +0200

    Передача файлов на удалённую машину, получение почты на встроенный в тесты почтовый сервер

--- mantis-tests/src/test/java/ru/stqa/mantis/appmanager/ApplicationManager.java
+++ mantis-tests/src/test/java/ru/stqa/mantis/appmanager/ApplicationManager.java
@@ -19,6 +19,8 @@ public class ApplicationManager {
     private String browser;
     private WebDriver wd;
     private RegistrationHelper registrationHelper;
+    private FtpHelper ftp;
+    private MailHelper mailHelper;
 
     public ApplicationManager(String browser) {
         this.browser = browser;
@@ -49,6 +51,13 @@ public class ApplicationManager {
         return registrationHelper;
     }
 
+    public FtpHelper ftp() {
+        if (ftp == null) {
+            ftp = new FtpHelper(this);
+        }
+        return ftp;
+    }
+
     public WebDriver getDriver() {
         if (wd == null) {
             if (Browser.CHROME.browserName().equals(browser)) {
@@ -62,5 +71,12 @@ public class ApplicationManager {
             wd.get(properties.getProperty("web.baseUrl"));
         }
         return wd;
+    }
+
+    public MailHelper mail() {
+        if (mailHelper == null) {
+            mailHelper = new MailHelper(this);
+        }
+        return mailHelper;
     }
 }
