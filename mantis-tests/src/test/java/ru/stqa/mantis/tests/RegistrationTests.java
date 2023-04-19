a84948f9ecb8dec2f18cf83ce9dc5113fc932f8a
Nataliya <khmelnitskayanatali@gmail.com>
2023-04-19 16:10:25 +0200

    Получение почты на внешний почтовый сервер.

--- mantis-tests/src/test/java/ru/stqa/mantis/tests/RegistrationTests.java
+++ mantis-tests/src/test/java/ru/stqa/mantis/tests/RegistrationTests.java
@@ -7,6 +7,7 @@ import org.testng.annotations.Test;
 import ru.lanwen.verbalregex.VerbalExpression;
 import ru.stqa.mantis.model.MailMessage;
 
+import javax.mail.MessagingException;
 import java.io.IOException;
 import java.util.List;
 
@@ -14,20 +15,22 @@ import static org.testng.Assert.assertTrue;
 
 public class RegistrationTests extends TestBase{
 
-    @BeforeMethod
+   // @BeforeMethod
     public void startMailServer() {
         app.mail().start();
     }
 
 
     @Test
-    public void testRegistration() throws IOException {
+    public void testRegistration() throws IOException, MessagingException {
         long now = System.currentTimeMillis();
         String user = "user" + now;
         String password = "password";
         String email = String.format("user%s@localhost", now);
+        app.james().createUser(user, password);
         app.registration().start(user, email);
-        List<MailMessage> mailMessages = app.mail().waitForMail(1, 10000);
+        //List<MailMessage> mailMessages = app.mail().waitForMail(1, 10000);
+        List<MailMessage> mailMessages = app.james().waitForMail(user, password, 6000);
         String confirmationLink = findConfirmationLink(mailMessages, email);
         app.registration().finish(confirmationLink, password);
         assertTrue(app.newSession().login(user, password));
@@ -42,7 +45,7 @@ public class RegistrationTests extends TestBase{
         return regex.getText(mailMessage.text);
     }
 
-    @AfterMethod(alwaysRun = true)
+   // @AfterMethod(alwaysRun = true)
     public void stopMailServer() {
         app.mail().stop();
     }
